62f6dcba8fef303c566eec6c3eaf4976f6d53fd0
chian <chian@LAPTOP-0QFBVVVR>
2022-05-27 11:42:16 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		if (a == 0 || b == 0 ) {
+			
+			throw new IllegalArgumentException();
+		}
+		else {
+			
+			return a / b; 
+		}
+	}
+}
